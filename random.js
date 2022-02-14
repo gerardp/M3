4a6d8054b5124e793723922c14af8a457a5f962b
gerardp <gporto@gmail.com>
2022-02-14 13:25:13 +0100

    Update random.js

--- random.js
+++ random.js
@@ -16,3 +16,12 @@ Math.floor(Math.random() * 100);
 
 // Returns a random integer from 0 to 100:
 Math.floor(Math.random() * 101);
+
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
