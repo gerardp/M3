6f0e825bfbfa2bf1b75f6da1840ed0470b28ce60
gerardp <gporto@gmail.com>
2022-02-14 12:53:51 +0100

    Create random.js

---
+++ random.js
@@ -0,0 +1,18 @@
+var num = Math.random();
+console.log(num);
+
+var num10 = num * 10;
+console.log(num10);
+
+var numRedondo = Math.floor(num10); 
+console.log(numRedondo);
+
+
+// Returns a random integer from 1 to 10:
+Math.floor(Math.random() * 10) + 1;
+
+// Returns a random integer from 0 to 99:
+Math.floor(Math.random() * 100);
+
+// Returns a random integer from 0 to 100:
+Math.floor(Math.random() * 101);
